49be77f6f6d4a9f11d4913819e32bf8278398cb4
Tammy Metz <tammy@tammymetz.com>
2019-01-02 13:23:43 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,20 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+
+  end
+
+
+  def create
+
   end
 
 end
